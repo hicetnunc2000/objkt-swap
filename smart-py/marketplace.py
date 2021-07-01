dc81dcde5146bf295c4e026a7ac142f86621a2b2
crzypatchwork <costabraba93@gmail.com>
2021-07-01 15:23:35 -0300

    final v2

--- smart-py/marketplace.py
+++ smart-py/marketplace.py
@@ -1,16 +1,18 @@
+        
 class Marketplace(sp.Contract):
-    def __init__(self, objkt, metadata, manager):
+    def __init__(self, objkt, metadata, manager, fee):
         self.init(
             objkt = objkt,
             metadata = metadata,
             manager = manager,
             swaps = sp.big_map(tkey=sp.TNat, tvalue=sp.TRecord(issuer=sp.TAddress, objkt_amount=sp.TNat, objkt_id=sp.TNat, xtz_per_objkt=sp.TMutez, royalties=sp.TNat, creator=sp.TAddress)),
-            counter = 500000
+            counter = 500000,
+            fee = fee
             )
             
     @sp.entry_point
     def swap(self, params):
-        sp.verify(params.objkt_amount > 0)
+        sp.verify((params.objkt_amount > 0) & ((params.royalties >= 0) | (params.royalties <= 250)))
         self.fa2_transfer(self.data.objkt, sp.sender, sp.self_address, params.objkt_id, params.objkt_amount)
         self.data.swaps[self.data.counter] = sp.record(issuer=sp.sender, objkt_amount=params.objkt_amount, objkt_id=params.objkt_id, xtz_per_objkt=params.xtz_per_objkt, royalties=params.royalties, creator=params.creator)
         self.data.counter += 1
@@ -26,8 +28,8 @@ class Marketplace(sp.Contract):
             self.amount = sp.fst(sp.ediv(self.data.swaps[params.swap_id].xtz_per_objkt, sp.mutez(1)).open_some())
                 
             # calculate fees and royalties
-            self.fee = self.amount * (self.data.swaps[params.swap_id].royalties + 25) / 1000
-            self.royalties = self.data.swaps[params.swap_id].royalties * self.fee / (self.data.swaps[params.swap_id].royalties + 25)
+            self.fee = self.amount * (self.data.swaps[params.swap_id].royalties + self.data.fee) / 1000
+            self.royalties = self.data.swaps[params.swap_id].royalties * self.fee / (self.data.swaps[params.swap_id].royalties + self.data.fee)
             
             # send royalties to NFT creator
             sp.send(self.data.swaps[params.swap_id].creator, sp.utils.nat_to_mutez(self.royalties))
@@ -36,7 +38,7 @@ class Marketplace(sp.Contract):
             sp.send(self.data.manager, sp.utils.nat_to_mutez(abs(self.fee - self.royalties)))
                 
             # send value to issuer
-            sp.send(self.data.swaps[params.swap_id].issuer, sp.amount -  sp.utils.nat_to_mutez(self.fee))
+            sp.send(self.data.swaps[params.swap_id].issuer, sp.amount - sp.utils.nat_to_mutez(self.fee))
         
         self.data.swaps[params.swap_id].objkt_amount = sp.as_nat(self.data.swaps[params.swap_id].objkt_amount - 1)
         
@@ -47,6 +49,11 @@ class Marketplace(sp.Contract):
         sp.verify((sp.sender == self.data.swaps[params].issuer) & (self.data.swaps[params].objkt_amount != 0))
         self.fa2_transfer(self.data.objkt, sp.self_address, sp.sender, self.data.swaps[params].objkt_id, self.data.swaps[params].objkt_amount)
         del self.data.swaps[params]
+    
+    @sp.entry_point
+    def update_fee(self, params):
+        sp.verify(sp.sender == self.data.manager)
+        self.data.fee = params
         
     @sp.entry_point
     def update_manager(self, params):
